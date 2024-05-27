f2b73b5f0c826140a868d1353b70c8517176dfd0
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-05-27 21:46:03 +0100

    team2database.js changes

--- db/team2Database.js
+++ db/team2Database.js
@@ -1,91 +1,71 @@
-const Dbase = require('./team2Dbase.js');
+const Dbase = require('./Database.js');
 
-class team2Dbase extends Dbase {
+class Team2Dbase extends Database {
     constructor(options) {
         super(options);
     }
 }
 
-getUnit () {
-    return new Promise((resolve, reject) => {
-        this.db.query('SELECT *FROM department', (err, results) => {
-        if (err) {
-        reject(err);
-        return;
-        }
-        resolve (results);
+getUnits() {
+  return new Promise((resolve, reject) => {
+    this.db.query('SELECT * FROM department', 
+    (err, results) => {
+      if (err) reject(err);
+      else resolve(results.rows);
     });
   });
-};
+}
 
-getpositions () {
+
+getPositions () {
  return new Promise((resolve, reject) => {
   this.db.query(
-    "SELECT position.id, position.title, position.salary, department.name as department_name FROM position INNER JOIN department ON position.department_id = department.id",
-     (err, results) => {
-       if (err) {
-        reject(err);
-        }
-        resolve (results);
-      }
-    );
+    `SELECT position.id, position.title, position.salary, department.name AS department_name 
+    FROM position 
+    INNER JOIN department ON position.department_id = department.id`,
+    (err, results) => {
+      if (err) reject(err);
+      else resolve(results.rows);
+    });
   });
 }
 
 getWorker() {
     return new Promise((resolve, reject) => {
         this.db.query(
-        `SELECT 
-        worker.id,
-        CONCAT (worker.first_nm,' ', worker.first_nm) as name,
-        position.title as position_title,
-        position.salary as position_salary,
-        department.name as department_name,
-        IF(CONCAT(manager.first_nm,' ', manager.first_nm) IS NULL, '',CONCAT(manager.first_nm, ' ', manager.first_nm)) as manager_name
-
-        FROM worker
-        INNER JOIN position ON worker.position_id = position.id
-        INNER JOIN department ON position.department_id = department.id
-        LEFT JOIN worker as manager ON worker.manager_id = manager.id`
-        ,(err, results) => {
-        if (err) {
-        reject(err);
-        }
-        resolve (results);
-           }
-        );
+               `SELECT worker.id, 
+                    CONCAT(worker.first_nm, ' ', worker.last_nm) AS name, 
+                    position.title AS position_title, 
+                    position.salary AS position_salary, 
+                    department.name AS department_name, 
+                    COALESCE(CONCAT(manager.first_nm, ' ', manager.last_nm), '') AS manager_name
+             FROM worker
+             INNER JOIN position ON worker.position_id = position.id
+             INNER JOIN department ON position.department_id = department.id
+             LEFT JOIN worker AS manager ON worker.manager_id = manager.id`,
+      (err, results) => {
+      if (err) reject(err);
+      else resolve(results.rows);
     });
-}  
-    
+  }); 
+}
 
 addDepartment(department) {
  return new Promise((resolve, reject) => {
-  this.db.query('INSERT INTO department SET ?', { name: department.department_name }, (err, results) => {
-   if (err) {
-    reject(err);
-} else {
- resolve(`Department ${department.department_name} added successfully`);
-   }
+  this.db.query('INSERT INTO department(name) VALUES ($1) RETURNING *', { name: department.department_name }, (err, results) => {
+   if (err) reject(err);
+ else resolve(`Department ${department.department_name} added successfully`);
   });
  });
 }
 
-
-
-addposition(position) {
-const positionData = {
-    title: position.title,
-    salary: position.salary,
-    department_id: position.department_id
-};
-
+addPosition(position) {
 return new Promise((resolve, reject) => {
- this.db.query('INSERT INTO position SET ?', positionData, (err, results) => {
-  if (err) {
-   reject(err);
-    } else {
-     resolve(`position ${position.title} added successfully`);
-    }
+  const { title, salary, department_id } = worker;
+  this.db.query('INSERT INTO position (title, salary, department_id ) VALUES ($1, $2, $3) RETURNING *',
+    [title, salary, department_id] (err, results) => {
+    if (err) reject(err);
+     else resolve(`Position ${title} added successfully`);
   });
  });
 }
@@ -98,15 +78,13 @@ const workerData = {
     manager_id: worker.manager_id
 }; 
 return new Promise((resolve, reject) => {
- this.db.query('INSERT INTO worker SET ?', workerData, (err, results) => {
-    if (err) {
-        reject(err);
-    } else {
-        resolve(`${worker.first_nm} ${worker.first_nm} added successfully`);
-        }
-       });
-      });
-     }
+  this.db.query('INSERT INTO worker (first_nm, last_nm, position_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *',
+  [first_nm, last_nm, position_id, manager_id] (err, results) => {
+  if (err) reject(err);
+   else resolve(`${first_nm} ${last_nm} added successfully`);
+});
+});
+}
 
 UpdateWorkerposition(worker) {
  return new Promise((resolve, reject) => {
