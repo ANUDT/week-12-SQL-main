c0244e21a8b58eb4bd5191a9608bfe4dc4890ce0
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-05-27 18:06:22 +0100

    schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,22 +5,15 @@ USE DATABASE team2_db:
 
 \c team2_db;
 
-CREATE TABLE team2_db (
+CREATE TABLE department (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(35) NOT NULL
 )
-CREATE TABLE department
-(
-  id INT NOT NULL 
-  AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR (30)
-);
 
-CREATE TABLE position
-(
-  id INT NOT NULL
-  AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR (30),
+CREATE TABLE position (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(35) 
+  title VARCHAR (35),
   salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id)
@@ -28,15 +21,12 @@ CREATE TABLE position
   on DELETE SET NULL
 );
 
-CREATE TABLE worker
-(
-  id INT NOT NULL
-  AUTO_INCREMENT PRIMARY KEY,
-  first_nm VARCHAR (30),
-  first_nm VARCHAR (30),
+CREATE TABLE worker (
+  id SERIAL PRIMARY KEY,
+  first_nm VARCHAR (35),
+  first_nm VARCHAR (35),
   position_id INT,
   manager_id INT,
-  FOREIGN KEY (position_id)
-  REFERENCES position (id)
-  on DELETE SET NULL
+  FOREIGN KEY (position_id) REFERENCES position (id) on DELETE SET NULL
+  FOREIGN KEY (position_id) REFERENCES position (id) on DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,24 @@
-INSERT INTO department (id, name) VALUES (1,'IT');
-INSERT INTO department (id, name) VALUES (2,'HR');
-INSERT INTO department (id, name) VALUES (3,'SALES');
-INSERT INTO department (id, name) VALUES (4,'FINANCE');
-INSERT INTO department (id, name) VALUES (5,'TRANSPORT');
+INSERT INTO department (id, name) VALUES (1,'IT'), (2,'HR'), (3,'SALES'), (4,'FINANCE'), (5,'TRANSPORT');
 
-INSERT INTO position  (id, title, salary,  department_id) VALUES (1,'Full Stack', 60000, 1);
-INSERT INTO position  (id, title, salary,  department_id) VALUES (2,'HR Advisor', 30000, 2);
-INSERT INTO position  (id, title, salary,  department_id) VALUES (3,'HR BP', 50000, 2);
-INSERT INTO position  (id, title, salary,  department_id) VALUES (4,'Sales Coordinator', 30000, 3);
-INSERT INTO position  (id, title, salary,  department_id) VALUES (5,'Accountant', 70000, 4);
-INSERT INTO position  (id, title, salary,  department_id) VALUES (6,'Transport Lead', 65000, 5);
 
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (1,'Micheal', 'Bolton', 1, NULL);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (2,'Nicky', 'Minaj', 5, 6);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (3,'Diana', 'Ross', 4, 3);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (4,'Elvis','Presley', 6, NULL);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (5,'Madonna', 'Madonna', 5, NULL);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (6,'Whitney', 'Houston', 1, NULL);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (7,'Sean', 'Paul', 2, 1,);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (8,'Daddy', 'Yankee', 3, 7);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (9,'Cliff', 'Richards', 4, NULL);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (10,'Tina', 'Turner', 2, NULL);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (11,'Janet', 'Jackson', 5, 6);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (12,'Adriana', 'Grande', 3, 4);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (13,'Jennifer', 'Lopez', 1, 2);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (14,'Shakira', 'Shakira', 6, NULL);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (15,'Shabba', 'Ranks', 3, 4);
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES (16,'Bad', 'BoltonBunny', 4, 6);
+INSERT INTO position  (id, title, salary,  department_id) VALUES 
+(1,'Full Stack', 60000, 1), (2,'HR Advisor', 30000, 2), (3,'HR BP', 50000, 2), (4,'Sales Coordinator', 30000, 3), (5,'Accountant', 70000, 4), (6,'Transport Lead', 65000, 5);
 
-module.exports = new DB();
+INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES 
+(1,'Micheal', 'Bolton', 1, NULL),
+(2,'Nicky', 'Minaj', 5, 6),
+(3,'Diana', 'Ross', 4, 3),
+(4,'Elvis','Presley', 6, NULL),
+(5,'Madonna', 'Madonna', 5, NULL),
+(6,'Whitney', 'Houston', 1, NULL),
+(7,'Sean', 'Paul', 2, 1,),
+(8,'Daddy', 'Yankee', 3, 7),
+(9,'Cliff', 'Richards', 4, NULL),
+(10,'Tina', 'Turner', 2, NULL),
+(11,'Janet', 'Jackson', 5, 6),
+(12,'Adriana', 'Grande', 3, 4),
+(13,'Jennifer', 'Lopez', 1, 2),
+(14,'Shakira', 'Shakira', 6, NULL),
+(15,'Shabba', 'Ranks', 3, 4),
+(16,'Bad', 'BoltonBunny', 4, 6);
+
