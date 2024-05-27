0c67b424a8cd4165b7de7b8fe7eb6bcd54e07ac4
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-05-27 18:11:27 +0100

    connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   host: 'localhost',
   user: 'postgres',
   password: process.env.DB_PASSWORD || 'Happy',
-  Dbase: 'team2',
+  database: 'team2',
   port: 5432 // Default PostgreSQL port
 });
 
