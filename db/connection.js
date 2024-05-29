754ef5bed0aa7bccd8cf1a5623952205b2107da9
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-05-29 17:41:53 +0100

    changed syntax in code

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: process.env.DB_PASSWORD || 'Happy',
+  host: "localhost",
+  user: "postgres",
+  password: process.env.DB_PASSWORD || "password",
   database: 'team2_db',
   port: 5432 // Default PostgreSQL port
 });
