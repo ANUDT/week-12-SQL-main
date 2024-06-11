26876f4dbe3dc603ea090e354bd988585c01ca0f
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-11 19:45:43 +0100

    seeds fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,9 +4,9 @@ INSERT INTO department (id, name) VALUES (1,'IT'), (2,'HR'), (3,'SALES'), (4,'FI
 INSERT INTO position  (id, title, salary,  department_id) VALUES 
 (1,'Full Stack', 60000, 1), (2,'HR Advisor', 30000, 2), (3,'HR BP', 50000, 2), (4,'Sales Coordinator', 30000, 3), (5,'Accountant', 70000, 4), (6,'Transport Lead', 65000, 5);
 
-INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES 
+INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id, department_id) VALUES 
 (1,'Micheal', 'Bolton', 1, NULL),
-(2,'Nicky', 'Minaj', 5, 6),
+(2,'Jan', 'Tommy', 5, 6),
 (3,'Diana', 'Ross', 4, 3),
 (4,'Elvis','Presley', 6, NULL),
 (5,'Madonna', 'Madonna', 5, NULL),
@@ -18,7 +18,7 @@ INSERT INTO worker  (id, first_nm, first_nm, position_id, manager_id) VALUES
 (11,'Janet', 'Jackson', 5, 6),
 (12,'Adriana', 'Grande', 3, 4),
 (13,'Jennifer', 'Lopez', 1, 2),
-(14,'Shakira', 'Shamira', 6, NULL),
-(15,'Shabba', 'Ranks', 3, 4),
+(14,'Sandy', 'Hallo', 6, NULL),
+(15,'Baby', 'Ranks', 3, 4),
 (16,'Bad', 'BoltonBunny', 4, 6);
 
