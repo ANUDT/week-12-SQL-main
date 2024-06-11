f047d445190c091ccabb222255ac639b2cdef51b
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-11 19:33:17 +0100

    Changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,29 @@
 DROP DATABASE IF EXISTS team2_db;
 CREATE DATABASE team2_db;
 
-USE DATABASE team2_db:
-
-\c team2_db;
+USE team2_db;
 
 CREATE TABLE department (
   id SERIAL PRIMARY KEY,
   name VARCHAR(35) NOT NULL
-)
+);
 
 CREATE TABLE position (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(35) 
   title VARCHAR (35),
   salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department (id)
-  on DELETE SET NULL
+  ON DELETE SET NULL
 );
 
 CREATE TABLE worker (
   id SERIAL PRIMARY KEY,
   first_nm VARCHAR (35),
-  first_nm VARCHAR (35),
+  last_nm VARCHAR (35),
   position_id INT,
   manager_id INT,
-  FOREIGN KEY (position_id) REFERENCES position (id) on DELETE SET NULL
-  FOREIGN KEY (department_id) REFERENCES department_id (id) on DELETE SET NULL
+  FOREIGN KEY (position_id) REFERENCES position (id) ON DELETE SET NULL,
+  FOREIGN KEY (department_id) REFERENCES department_id (id) ON DELETE SET NULL
 );
